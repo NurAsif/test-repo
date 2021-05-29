5ac852607472aed8b7794ab8b7dac073cf06be05
NurAsif <nur15-14370@diu.edu.bd>
2021-05-29 11:14:42 +0600

    add flies

---
+++ hello.c
@@ -0,0 +1,10 @@
+// this is a basic hello world program
+#include <stdio.h>
+
+// function main program execution
+int main()
+{
+	printf("Hello, World!\n"); // display
+	
+	return 0; // indicates program run successfully 
+} // end of function main
